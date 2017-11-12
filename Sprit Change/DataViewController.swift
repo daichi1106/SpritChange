af4482a2490d8cf78c9e36f58e3e76466feaf126
早見大地 <daihmy1106@gmail.com>
2017-11-12 12:15:59 +0900

    Initial Commit

---
+++ Sprit Change/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  Sprit Change
+//
+//  Created by 早見　大地 on 2017/11/12.
+//  Copyright © 2017年 早見　大地. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
---
+++ Sprit ChangeTests/Sprit_ChangeTests.swift
@@ -0,0 +1,36 @@
+//
+//  Sprit_ChangeTests.swift
+//  Sprit ChangeTests
+//
+//  Created by 早見　大地 on 2017/11/12.
+//  Copyright © 2017年 早見　大地. All rights reserved.
+//
+
+import XCTest
+@testable import Sprit_Change
+
+class Sprit_ChangeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
